c2c5b1a1f121ebb31908220ee5a50626e7a58c5a
weiwei.xing <weiwei.xing@soulinfo.com>
2015-04-10 15:50:44 +0800

    tune

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
 	var percentage=parseInt($(".newbadhabbit_percentage").text());
 	$(".progressbar span").css("width",percentage+"%")
 	$(".newbadhabbit_title_wrapper a, .newbadhabbit_dialog_wrapper input[type=button]").click(function(){
@@ -8,4 +9,65 @@ $(document).ready(function(){
 			setTimeout(function(){$(".newbadhabbit_dialog_wrapper").fadeOut();},300)
 		}
 	})
+
+
+
+	$(".slideTxtBox").slide();
+
+
+	$('#happy-carousel').carousel({
+		interval: 4000
+	});
+
+
+	$('[id^=carousel-selector-]').click( function(){
+		var id_selector = $(this).attr("id");
+		var id = id_selector.substr(id_selector.length -1);
+		id = parseInt(id);
+		$('#happy-carousel').carousel(id);
+		$('[id^=carousel-selector-]').removeClass('selected');
+		$(this).addClass('selected');
+	});
+
+
+	$('[id^=carousel-selector-]').hover(function(){
+		var id_selector = $(this).attr("id");
+		var id = id_selector.substr(id_selector.length -1);
+		id = parseInt(id);
+		$('#happy-carousel').carousel(id);
+		$('[id^=carousel-selector-]').removeClass('selected');
+		$(this).addClass('selected');
+	});
+
+
+	$('#happy-carousel').on('slid', function (e) {
+		var id = $('.item.active').data('slide-number');
+		id = parseInt(id);
+		$('[id^=carousel-selector-]').removeClass('selected');
+		$('[id=carousel-selector-'+id+']').addClass('selected');
+	});
+
+	var changeUserLevelDiskImg = function(imgUrl) {
+		$("#user-level-disk-img").attr("src", imgUrl);
+	}
+
+	$("#user-level-test").hover(function() {
+		changeUserLevelDiskImg("img/test-round.png")
+	}, function(){
+		changeUserLevelDiskImg("img/half-round.png");
+	})
+
+	$("#user-level-exercise").hover(function() {
+		changeUserLevelDiskImg("img/exercise-round.png")
+	}, function(){
+		changeUserLevelDiskImg("img/half-round.png");
+	})
+
+	$("#user-level-mark").hover(function() {
+		changeUserLevelDiskImg("img/mark-round.png")
+	}, function(){
+		changeUserLevelDiskImg("img/half-round.png");
+	})
+
+
 })
