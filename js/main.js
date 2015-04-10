b4e4534d45e12ef630e9a64c57d31dccd5353fd9
weiwei.xing <weiwei.xing@soulinfo.com>
2015-04-10 18:34:09 +0800

    快乐演习首页

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){
+	happyexecrise();
+})
+function happyexecrise(){
+	var index=0;
 	var percentage=parseInt($(".newbadhabbit_percentage").text());
 	$(".progressbar span").css("width",percentage+"%")
 	$(".newbadhabbit_title_wrapper a, .newbadhabbit_dialog_wrapper input[type=button]").click(function(){
@@ -8,4 +12,14 @@ $(document).ready(function(){
 			setTimeout(function(){$(".newbadhabbit_dialog_wrapper").fadeOut();},300)
 		}
 	})
-})
+
+	$(".haexe_home_ranking_list_wrapper li").click(function(){
+		index=$(this).index();
+		$(".haexe_home_carousel_tree_wrapper div").fadeOut().eq(index).fadeIn();
+		$(this).siblings("li").removeClass("shown");
+		$(this).addClass("shown");
+	})
+	$(".haexe_home_carousel_desc_list ul li").click(function(){
+		console.log("aaaq")
+	})
+}
