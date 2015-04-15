b13525daecfe1f07b23ac30b48a2c94776ca410c
unknown <sunweihong0520@sina.com>
2015-04-15 15:08:26 +0800

    add hp test

---
+++ js/hp_test.js
@@ -0,0 +1,6 @@
+    $('.test_questions input').iCheck({
+        checkboxClass: 'icheckbox_minimal-green',  // 注意square和blue的对应关系,用于type=checkbox
+        radioClass: 'iradio_minimal-green', // 用于type=radio
+        increaseArea: '20%' // 增大可以点击的区域
+    });
+
