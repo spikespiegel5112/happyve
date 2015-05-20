916775023ddeef47d72da59e7bb6141b24fef9f7
wangbolun <weiwei.xing@soulinfo.com>
2015-05-20 10:08:52 +0800

    temp

--- js/28daychangemyself.js
+++ js/28daychangemyself.js
@@ -3,25 +3,20 @@
 	function changemyself(){
 		var tdElArr=[],initGrid=[28];
 		var changeCount=0;
-		var imgsrc1=$('.changemyself_content_img img').eq(0).attr('src');
-		var imgsrc2=$('.changemyself_content_img img').eq(1).attr('src');
+		// var imgsrc1=$('.changemyself_content_img img').eq(0).attr('src');
+		// var imgsrc2=$('.changemyself_content_img img').eq(1).attr('src');
 		var gridEl='';
 		var classname='changemyself_grid_wrapper_1';
 		var badhabbitLiEl=$('.newbadhabbit_content_item li');
 		var imgsrc={};
 		var imgsrcObj={'imgsrc':[]};
 		init();
-		for(var i=0;i<badhabbitLiEl.length;i++){
-			imgsrc1=$('.changemyself_content_img img').eq(i).attr('src');
-			imgsrc2=$('.changemyself_content_img img').eq(i+1).attr('src');
-			imgsrc={'imgsrc1':imgsrc1,'imgsrc2':imgsrc2}
-			imgsrcObj.imgsrc.push(imgsrc);
-		}
+		
 		function init(){
 			bulidgrid(classname);
-			showgrid(imgsrc1,classname);
+			showgrid(classname);
 			generateRandom();
-			console.log(imgsrcObj)
+			console.log(imgsrcObj.imgsrc[0].imgsrc1);
 		}
 		
 		function bulidgrid(classname){
@@ -35,14 +30,24 @@
 			}
 			gridEl+='</tbody></table>'
 			$('.changemyself_content_img').prepend(gridEl);
-
 		}
-		function showgrid(imgsrc,classname){
-			var gridWidth=$('.changemyself_grid_wrapper_1').width();
-			var gridHeight=$('.changemyself_grid_wrapper_1').height();
-			tdEl=$('.'+classname).eq(0).find('td');
+		function showgrid(classname){
+			var gridWidth=$('.'+classname).width();
+			var gridHeight=$('.'+classname).height();
+			for(var i=0;i<badhabbitLiEl.length;i++){
+				imgsrc1=$('.changemyself_content_img img').eq(i).attr('src');
+				imgsrc2=$('.changemyself_content_img img').eq(i+1).attr('src');
+				imgsrc={'imgsrc1':imgsrc1,'imgsrc2':imgsrc2}
+				imgsrcObj.imgsrc.push(imgsrc);
+				i++;
+			}
+			for(var i=0;i<$('.newbadhabbit_content_item li').length;i++){
+				tdEl=$('.'+classname).eq(i).find('td');
+			}
+			
 			var trIndex=0,tdElIndex=0,posX=0,posY=0;
-			tdEl.each(function(){$(this).find('div').css({'background-image':'url(./'+imgsrc1+')','background-size':gridWidth+"px "+gridHeight+"px"})});
+			
+			tdEl.each(function(){$(this).find('div').css({'background-image':'url(./'+imgsrcObj.imgsrc[0].imgsrc1+')','background-size':gridWidth+"px "+gridHeight+"px"})});
 			for(row=0;row<4;row++){
 				trIndex=row*7;
 				posY=row*gridHeight/4;
@@ -52,7 +57,7 @@
 					tdEl.eq(tdElIndex).find('div').css({'background-position':'-'+posX+'px -'+posY+'px'});
 				}
 			}
-			tdEl.css({'width':gridWidth/7,'height':gridHeight/4})
+			tdEl.css({'width':gridWidth/7,'height':gridHeight/4});
 		}
 		function generateRandom(){
 			var cellCount=28;
