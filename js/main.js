2f2939d82f17543da620622bfe6103064d1529b6
unknown <sunweihong0520@sina.com>
2015-05-25 13:53:01 +0800

    add myfriend detail page and modify bug

--- js/main.js
+++ js/main.js
@@ -36,11 +36,11 @@
     function layout(){
         $(".nav_main_list_item").eq(0).hover(function(){
             var dropdownEl=$(this).siblings(".nav_dropdown_item");
-            $(".nav_dropdown_item").show();
+            $(".nav_dropdown_item").show(300);
             $(this).find("ul").addClass("dropdown_fadein");
         },function(){
             var dropdownEl=$(this).siblings(".nav_dropdown_item");
-            $(".nav_dropdown_item").hide();
+            $(".nav_dropdown_item").hide(300);
             $(this).find("ul").removeClass("dropdown_fadein");
         });
     }
